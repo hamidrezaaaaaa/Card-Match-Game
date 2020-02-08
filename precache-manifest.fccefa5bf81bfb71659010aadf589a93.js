893d97b25aa0afff3e4f44eef8907b5c0f06f964
kaz-yamada <kaz.yamada@outlook.com>
2020-02-08 12:08:49 +1100

    Updates

---
+++ precache-manifest.fccefa5bf81bfb71659010aadf589a93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461c85688818c0a55abf5aa652961a9c",
+    "url": "/Card-Match-Game/index.html"
+  },
+  {
+    "revision": "8b65e3bc369e46716a2c",
+    "url": "/Card-Match-Game/static/js/2.d7abcce0.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/Card-Match-Game/static/js/2.d7abcce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c43bf725a5d40bc636e",
+    "url": "/Card-Match-Game/static/js/main.0ca4e507.chunk.js"
+  },
+  {
+    "revision": "d3f507c98d04c8d61b76",
+    "url": "/Card-Match-Game/static/js/runtime-main.4c5b5c84.js"
+  }
+]);
